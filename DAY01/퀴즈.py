6b2ebc0c1a694ce8a4a3997de7fa1f46eca4bdcd
tjsdn0610 <155503310+tjsdn0610@users.noreply.github.com>
2024-01-03 12:50:21 +0900

    Add files via upload

---
+++ DAY01/퀴즈.py
@@ -0,0 +1,13 @@
+#자기소개
+#이름 입력:
+#mbti 입력:
+#좋아하는 음식 입력:
+#결과=> 이름이 ~~이시고, mbti가 ~~이시고, 좋아하는 음식이 ~~이군요t("이름 입력:")
+name = input("이름 입력:")
+mbti = input("mbti 압력:")
+food = input("최애 음식:")
+print(f"이름은 {name} mbti는 {mbti} 최애 음식은 {food} 이군요")
+
+movie = input("영화 입력:")
+ott  = input("ott 입력:")
+print(f"최근에 본 영화는 {movie} ott는 {ott}")
