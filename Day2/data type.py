872073322098d5c9a3cc180863322ee58dff53ea
tjsdn0610 <155503310+tjsdn0610@users.noreply.github.com>
2024-01-04 12:49:37 +0900

    Add files via upload

---
+++ Day2/data type.py
@@ -0,0 +1,20 @@
+#data type
+num1= int(input("첫번째 수"))
+
+num2 = int(input("두번째 수"))
+
+print(f"첫번째 수+두번째 수={num1+num2}")
+print(f"첫번째 수-두번째 수={num1-num2}")
+print(f"첫번째 수*두번째 수={num1*num2}")
+
+
+#input() : 유저한테 입력받는 기능
+#print() : 안에 들어있는 내용 출력
+#int() : 안에 들어있는 문자를 정수화
+
+
+a=10                #a는 숫자 정수형 타입
+b=3.14              #b는 숫자 실수형 타입
+c="megastudy"       #c는 문자형타입
+
+
---
+++ Day2/리뷰.py
@@ -0,0 +1,7 @@
+time = input("일어난 시간")
+food = input("먹은 메뉴")
+print(F"일어난 시간{time}, 먹은 메뉴는 {food}")
+# 연산(operator)
+# 1+2+3 : 사칙연산
+# time : 호출연산
+# and or not : 논리연산
